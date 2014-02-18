ebb5e539e963f6447fa5ca258a5218ed7ba462d2
LAU Thierry <thierlau@gmail.com>
2014-02-18 23:55:40 +0100

    initial commit

---
+++ app/index.js
@@ -0,0 +1,57 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var currentWorkingDirectory = process.cwd().split(path.sep).pop();
+
+
+var NgtailorGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.npmInstall();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // have Yeoman greet the user
+    console.log(this.yeoman);
+
+    // replace it with a short and sweet description of your generator
+    console.log(chalk.magenta('You\'re using the fantastic Ngtailor generator.'));
+
+    var prompts = [{
+      type: 'input',
+      name: 'name',
+      message: 'Name your project',
+      default: currentWorkingDirectory
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.mkdir('app');
+    this.mkdir('app/templates');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = NgtailorGenerator;
